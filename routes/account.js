da51e4880170d0542c62116317d065afb1870d0b
Oliver Ignetik <olli@ozemail.com.au>
2019-07-17 23:45:17 +1000

    User can add/remove items from wishlist

--- routes/account.js
+++ routes/account.js
@@ -18,20 +18,28 @@ router.get('/', (req, res) => {
         if (err) {
             return next(err)
         }
-        // set the user as data to pass to account ejs template
-        // reference object multiple times
-        const data = {
-            user: user,
-            items: items
-        }
 
-        // Pass render a user object
-        res.render('account', data);
+        // nested query to check for items you're interested in 
+        Item.find({
+            interested: user._id
+        }, (err, interestedItems) => {
+            if (err) return next(err);
+
+            const data = {
+                user: user,
+                items: items,
+                interested: interestedItems
+            }
+
+            // Pass render a object
+            res.render('account', data);
+
+        });
     });
 
 });
 
-router.get('/additem/:itemid', (req, res,next) => {
+router.get('/additem/:itemid', (req, res, next) => {
     // passport binds user to req.user
     const user = req.user;
 
@@ -42,16 +50,48 @@ router.get('/additem/:itemid', (req, res,next) => {
     }
 
     // grab the id
-    const itemid =req.params.itemid;
-    
-    Item.findById(itemid,(err,item)=>{
-    if(err) return next(err);
-    
-    if(item.interested.indexOf(user._id)==-1){
-        item.interested.push(user._id);
+    const itemid = req.params.itemid;
+
+    Item.findById(itemid, (err, item) => {
+        if (err) return next(err);
+
+        if (item.interested.indexOf(user._id) == -1) {
+            item.interested.push(user._id);
+            // save the item after you add the interested user id
+            item.save();
+            // redirect requires you specify full path
+            res.redirect('/account');
+        }
+
+        res.redirect('/account');
+
+    })
+});
+
+router.get('/removeitem/:itemid', (req, res, next) => {
+    // passport binds user to req.user
+    const user = req.user;
+
+    // just incase there is no user
+    if (user == null) {
+        res.redirect('/');
+        return;
+    }
+
+    // grab the id
+    const itemid = req.params.itemid;
+
+    Item.findById(itemid, (err, item) => {
+        if (err) return next(err);
+        // remove item from array
+        let index = item.interested.indexOf(user._id);
+        if (index > -1) {
+            item.interested.splice(index, 1);
+        }
         // save the item after you add the interested user id
         item.save();
-    }
+        // redirect requires you specify full path
+        res.redirect('/account');
 
     })
 });
